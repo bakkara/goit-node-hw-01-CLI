50c93f60bc18a627cdc1a664f42b95b95c59336c
bakkara <ane4karymar@gmail.com>
2023-11-03 00:01:37 +0200

    done

---
+++ README.md
@@ -0,0 +1,1 @@
+https://monosnap.com/list/6543fbed3b9bd77d4dac1b60
---
+++ contacts.js
@@ -0,0 +1,47 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { nanoid } from 'nanoid';
+
+const contactsPath = path.resolve('db', 'contacts.json')
+const updateContacts = contacts => fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+// Повертає масив контактів.
+
+export async function listContacts() {
+    const contacts = await fs.readFile(contactsPath, 'utf-8')
+    return JSON.parse(contacts);
+}
+
+// Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+export async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+}
+
+// Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+export async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((item) => item.id === contactId);
+    if (index === -1) {
+        return null;
+    }
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return result;
+}
+
+// Повертає об'єкт доданого контакту. 
+export async function addContact({name, email, phone}) {
+    const contacts = await listContacts();
+    const newContact = {
+        id: nanoid(),
+        name,
+        email,
+        phone
+    }
+    contacts.push(newContact);
+    await updateContacts(contacts);
+    return newContact;
+}
+
